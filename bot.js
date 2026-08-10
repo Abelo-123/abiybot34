fdd428365cc1530aadce20dd7d8bf6846e0aaf97
Abelo-123 <abeloabate01@gmail.com>
2026-08-10 15:28:26 +0300

    okokokokpp005*popo

--- bot.js
+++ bot.js
@@ -169,12 +169,16 @@ bot.onText(/\/start/, async (msg) => {
 
     // ✅ Send welcome image with Start App button (ALWAYS shown)
     try {
-        const welcomeText = ` <b>👋 Welcome to Primora444!</b> \n\n` +
-            `Grow your social media faster with high-quality, affordable services🚀\n\n` +
-            `Choose a service, place your order, and watch your account grow automatically. Thanks for choosing Ziviop!`;
+        const welcomeText = ` <b>🚀 Grow Bigger with Primora 444! 📈🔥</b> \n\n` +
+            `Followers 👥 • Views 👀 • Likes ❤️ • Shares 🔄 • Comments 💬\n\n` +
+            `✨ We Boost. You Grow. 📈\n\n` +
+            `Your growth is our mission. 🚀💙` +
+            `📲 Choose your service & let Primora 444 boost your social media presence!` +
+            `💰 Fair Prices • Real Growth 🔥
+ተመጣጣኝ ዋጋ — ትልቅ እድገት! 🌟🔥`;
 
 
-        await bot.sendPhoto(chatId, 'https://i.ibb.co/VcYG76kj/logo.jpg', {
+        await bot.sendPhoto(chatId, 'https://i.ibb.co/5hLKm1xy/o7.jpg', {
             caption: welcomeText,
             parse_mode: 'HTML',
             reply_markup: {
